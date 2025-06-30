2e24524b8168e13640a863f11c0c07ffee530cba
Lucas Paulo <lp152659@gmail.com>
2025-06-30 20:21:25 -0300

    Organização de controllers e implementação de dados de páginas

--- server/src/routes/Router.mjs
+++ server/src/routes/Router.mjs
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 import userRoute from './userRoutes.mjs';
+import pageRouter from './pageRoutes.mjs';
 
 router.use('/api/users', userRoute);
+router.use('/api/pages', pageRouter);
 
 export default router;
